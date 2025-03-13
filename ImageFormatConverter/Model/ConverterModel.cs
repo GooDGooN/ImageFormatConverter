26f5a3c8563b6e74d7e749dcd4ee44141c589ac1
GooDGooN <qwe91911@gmail.com>
2025-03-14 07:18:58 +0900

    변수 이름 변경, 변경가능포맷 추가, 변환 작동 되게 설정

--- ImageFormatConverter/Model/ConverterModel.cs
+++ ImageFormatConverter/Model/ConverterModel.cs
@@ -6,18 +6,30 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ImageFormatConverter.Model;
+
+public enum TargetImageFormat
+{
+    png,
+    gif,
+    jpeg,
+    bmp,
+    webp,
+    icon,
+}
 public class ConverterModel
 {
     public List<string> ImportedImageFiles = new();
     public string? SaveDirection;
     public ObservableCollection<string> ListItems = new();
-    public ObservableCollection<string> FormatItems = ["PNG", "GIF", "JPEG", "BMP"];
+    public ObservableCollection<string> FormatItems = ["PNG", "GIF", "JPEG", "BMP", "WEBP", "ICON"];
     public int TargetFormatIndex;
+    public bool IsCreateNewFolder = true;
 
 
     public ConverterModel()
     {
         ResetField();
+
     }
 
     public void ResetField()
--- ImageFormatConverter/ViewModel/ConverterViewModel.cs
+++ ImageFormatConverter/ViewModel/ConverterViewModel.cs
@@ -10,6 +10,7 @@ using System.Windows.Input;
 using ImageFormatConverter.Model;
 using System.Drawing;
 using Microsoft.Win32;
+using System.Drawing.Imaging;
 
 
 namespace ImageFormatConverter.ViewModel;
@@ -24,16 +25,22 @@ public class ConverterViewModel
     }
 
     public ICommand OnImportButton => new RelayCommand(FolderImport);
-    public ICommand OnConvertButton => new RelayCommand(ImagesConvert);
+    public ICommand OnExportButton => new RelayCommand(ExportImages);
 
 
     public ObservableCollection<string> GetItemsFromModel => model.ListItems;
     public ObservableCollection<string> GetFormatsFromModel => model.FormatItems;
 
-    public int SetFormatIndex
+    public int FormatIndex
     {
         get => model.TargetFormatIndex;
         set => model.TargetFormatIndex = value;
+    }
+
+    public bool IsCreateNewFolder
+    {
+        get => model.IsCreateNewFolder;
+        set => model.IsCreateNewFolder = value;
     }
 
     public ConverterViewModel()
@@ -76,9 +83,75 @@ public class ConverterViewModel
         dialog.ShowDialog();
         TryImportImages(dialog.FolderName);
     }
-    private void ImagesConvert()
+    private void ExportImages()
     {
-        
+        if (GetItemsFromModel.Count > 0)
+        {
+            var dialog = new OpenFolderDialog();
+            dialog.Title = "Select export location";
+            dialog.ShowDialog();
+            if(dialog.FolderName != "")
+            {
+                var dir = dialog.FolderName;
+                if (IsCreateNewFolder)
+                {
+                    dir += @"\ImageFormatConvert";
+                    Directory.CreateDirectory(dir);
+                }
+
+                foreach(var file in GetItemsFromModel)
+                {
+                    var format = ImageFormat.Png;
+                    var formatStr = ".png";
+                    var targetEnum = (TargetImageFormat)FormatIndex;
+                    switch (targetEnum)
+                    {
+                        case TargetImageFormat.gif:
+                            format = ImageFormat.Gif;
+                            formatStr = ".gif";
+                            break;
+                        case TargetImageFormat.jpeg: 
+                            format = ImageFormat.Jpeg;
+                            formatStr = ".jpeg";
+                            break;
+                        case TargetImageFormat.bmp: 
+                            format = ImageFormat.Bmp;
+                            formatStr = ".bmp";
+                            break;
+                        case TargetImageFormat.webp: 
+                            format = ImageFormat.Webp;
+                            formatStr = ".webp";
+                            break;
+                        case TargetImageFormat.icon: 
+                            format = ImageFormat.Icon;
+                            formatStr = ".icon";
+                            break;
+                    }
+                    var strLeftIndex = file.Length - 1;
+                    var strRightIndex = file.Length - 1;
+                    while (true)
+                    {
+                        if (file[strLeftIndex--] == 92)
+                        {
+                            break;
+                        }
+                        if (file[strRightIndex] != 46)
+                        {
+                            strRightIndex--;
+                        }
+                    }
+                    var fileName = dir + file.Substring(strLeftIndex + 1, strRightIndex - strLeftIndex - 1) + formatStr;
+                    MessageBox.Show(fileName);
+                    Image.FromFile(file).Save(fileName, format);
+                }
+
+                MessageBox.Show($"saved at {dir}");
+            }
+        }
+        else
+        {
+            MessageBox.Show($"There is nothing to convert!");
+        }
     }
 
     private bool IsImageFormat(string filePath)
